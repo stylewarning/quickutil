54459f8a247b51a71ec79ebfbda1ac2f5b856fbb
Robert Smith <quad@symbo1ics.com>
2012-10-29 13:07:41 -0500

    Add some real test utilities

--- quickutil-utilities/test.lisp
+++ quickutil-utilities/test.lisp
@@ -1,3 +1,7 @@
+;;;; Test utilities
+
+;;; Fake utilities
+
 (defutil :foo (:version (1 . 0)
                :depends-on (:bar :quux))
   (defun foo ()
@@ -24,3 +28,34 @@
 (defutil :robert (:version (22 . 0)
                   :depends-on (:eitarow))
   (defvar *robert* *eitarow*))
+
+
+;;; Real utilities, taken from QTILITY.
+
+(defutil :non-negative-p (:version (1 . 0))
+  (defun non-negative-p (n)
+    "Check if N is non-negative."
+    (declare (type real n))
+    (>= n 0)))
+
+(defutil :range (:version (1 . 0))
+  (defun range (start end &key (step 1) (key 'identity))
+    "Return the list of numbers n such that START <= n < END and n =
+START + k*STEP. If a function KEY is provided, then apply KEY to each
+number."
+    (assert (<= start end))
+    (loop :for i :from start :below end :by step :collecting (funcall key i))))
+
+(defutil :iota (:version (1 . 0)
+                :depends-on (:range :non-negative-p))
+  (defun iota (n)
+    "Return [0, ..., N-1]."
+    (assert (non-negative-p n))
+    (range 0 n)))
+
+(defutil :iota+1 (:version (1 . 0)
+                  :depends-on (:range))
+  (defun iota+1 (n)
+    "Return [1, ..., N]."
+    (assert (>= n 1))
+    (range 1 (1+ n))))
