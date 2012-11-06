8baf9151a2347af1a68f35faac23821d27d18953
Eitarow Fukamachi <e.arrows@gmail.com>
2012-11-06 22:27:18 +0900

    using jQuery.Pjax

--- quickutil-server/static/js/quickutil.js
+++ quickutil-server/static/js/quickutil.js
@@ -6,6 +6,30 @@ if (Quickutil.init) return;
 Quickutil.init = $.Deferred(function () { $(this.resolve); });
 
 Quickutil.init.done(function() {
+    $.pjax.defaults.timeout = 20 * 1000;
+    $.pjax.defaults.contentType = 'text/html';
+
+    $(document).on('click', 'a[data-pjax]', function(e) {
+        if (e.isDefaultPrevented()) return;
+        var target = $(this);
+        if (target.hasClass('disabled')) return;
+        $.pjax({
+            url: target.attr('href'),
+            fragment: target.attr('data-pjax'),
+            container: target.attr('data-pjax'),
+            target: target.get(0)
+        });
+        e.preventDefault();
+    });
+
+    $(document).on('click', '.menu li a', function(e) {
+        var target = $(e.currentTarget);
+        $('.menu li').removeClass('current');
+        target.closest('li').addClass('current');
+    });
+
+    $(document).on('pjax:success', function() { prettyPrint(); });
+
     var updateFilter = function() {
         if ($(this).length === 0) return;
 
