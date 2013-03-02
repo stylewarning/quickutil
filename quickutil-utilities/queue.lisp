bc3938457b8e6043e3773f84787326a0477d20c2
Robert Smith <quad@symbo1ics.com>
2013-03-02 01:56:32 -0800

    Add efficient queue implementation.

--- quickutil-utilities/queue.lisp
+++ quickutil-utilities/queue.lisp
@@ -2,23 +2,43 @@
 
 (in-package #:quickutil-utilities)
 
-;;; N.B. This is not an efficient queue implementation. DEQUEUE takes
-;;; O(N) time. Implementing a heap would be a better idea.
+;;; Taken from:
+;;; https://bitbucket.org/tarballs_are_good/lisp-random/raw/0a0dd60e64b92d033b2a30a10e97063b210a0152/stack-queue.lisp
 
-(defstruct queue
-  elements)
+(defstruct (queue (:constructor %make-queue)
+                  (:predicate queuep))
+  (elements nil :type list)
+  (last nil :type (or null (cons t null))))
+
+(defun make-queue ()
+  "Create a new empty queue."
+  (%make-queue))
 
 (defun queue-empty-p (queue)
+  "Is the queue QUEUE empty?"
   (null (queue-elements queue)))
 
-(defun enqueue (queue item)
-  (setf (queue-elements queue)
-        (push item (queue-elements queue))))
+(defun enqueue (queue obj)
+  "Add an element OBJ to the end of the queue QUEUE."
+  (let ((last (list obj)))
+    (if (queue-empty-p queue)
+        ;; Set up the queue with the first element. Note that the same
+        ;; reference to the singleton list is shared by both
+        ;; QUEUE-ELEMENTS and QUEUE-LAST.
+        (setf (queue-elements queue) last
+              (queue-last queue)     last)
+        
+        ;; We can now append elements to QUEUE-ELEMENTS simply by
+        ;; modifying QUEUE-LAST, whose reference is shared by
+        ;; QUEUE-ELEMENTS,
+        ;;
+        ;; We do this instead of a single SETF for type safety of
+        ;; QUEUE-LAST.
+        (let ((old (queue-last queue)))
+          (setf (queue-last queue) last
+                (cdr old)          last))))
+  queue)
 
 (defun dequeue (queue)
-  (let ((elts (queue-elements queue)))
-    (prog1 (if (null elts)
-               nil
-               (first (last elts)))
-      (setf (queue-elements queue)
-            (butlast elts)))))
+  "Remove and return an element from the queue QUEUE."
+  (pop (queue-elements queue)))
