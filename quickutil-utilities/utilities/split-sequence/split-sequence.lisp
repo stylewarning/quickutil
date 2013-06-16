aa7f1bef15e33c7eef5aaa48c68ead4882eba28a
Robert Smith <quad@symbo1ics.com>
2013-06-16 14:33:31 -0700

    Move around items from SPLIT-SEQUENCE to avoid warnings.

--- quickutil-utilities/utilities/split-sequence/split-sequence.lisp
+++ quickutil-utilities/utilities/split-sequence/split-sequence.lisp
@@ -5,6 +5,39 @@
                          :category (split-sequence sequences))
   "Split sequences into a list of subsequences based off of a delimiter or function."
   #>%%%>
+  (defun split-from-end (position-fn sequence start end count remove-empty-subseqs)
+    (loop
+      :for right := end :then left
+      :for left := (max (or (funcall position-fn sequence right) -1)
+                        (1- start))
+      :unless (and (= right (1+ left))
+                   remove-empty-subseqs) ; empty subseq we don't want
+        :if (and count (>= nr-elts count))
+          ;; We can't take any more. Return now.
+          :return (values (nreverse subseqs) right)
+      :else
+        :collect (subseq sequence (1+ left) right) into subseqs
+        :and :sum 1 :into nr-elts
+      :until (< left start)
+      :finally (return (values (nreverse subseqs) (1+ left)))))
+
+  (defun split-from-start (position-fn sequence start end count remove-empty-subseqs)
+    (let ((length (length sequence)))
+      (loop
+        :for left := start :then (+ right 1)
+        :for right := (min (or (funcall position-fn sequence left) length)
+                           end)
+        :unless (and (= right left)
+                     remove-empty-subseqs) ; empty subseq we don't want
+          :if (and count (>= nr-elts count))
+            ;; We can't take any more. Return now.
+            :return (values subseqs left)
+        :else
+          :collect (subseq sequence left right) :into subseqs
+          :and :sum 1 :into nr-elts
+        :until (>= right end)
+        :finally (return (values subseqs right)))))
+  
   (macrolet ((check-bounds (sequence start end)
                (let ((length (gensym (string '#:length))))
                  `(let ((,length (length ,sequence)))
@@ -90,37 +123,4 @@ stopped."
           (split-from-start (lambda (sequence start)
                               (position-if-not predicate sequence :start start :key key))
                             sequence start end count remove-empty-subseqs))))
-
-  (defun split-from-end (position-fn sequence start end count remove-empty-subseqs)
-    (loop
-      :for right := end :then left
-      :for left := (max (or (funcall position-fn sequence right) -1)
-                        (1- start))
-      :unless (and (= right (1+ left))
-                   remove-empty-subseqs) ; empty subseq we don't want
-        :if (and count (>= nr-elts count))
-          ;; We can't take any more. Return now.
-          :return (values (nreverse subseqs) right)
-      :else
-        :collect (subseq sequence (1+ left) right) into subseqs
-        :and :sum 1 :into nr-elts
-      :until (< left start)
-      :finally (return (values (nreverse subseqs) (1+ left)))))
-
-  (defun split-from-start (position-fn sequence start end count remove-empty-subseqs)
-    (let ((length (length sequence)))
-      (loop
-        :for left := start :then (+ right 1)
-        :for right := (min (or (funcall position-fn sequence left) length)
-                           end)
-        :unless (and (= right left)
-                     remove-empty-subseqs) ; empty subseq we don't want
-          :if (and count (>= nr-elts count))
-            ;; We can't take any more. Return now.
-            :return (values subseqs left)
-        :else
-          :collect (subseq sequence left right) :into subseqs
-          :and :sum 1 :into nr-elts
-        :until (>= right end)
-        :finally (return (values subseqs right)))))
   %%%)
