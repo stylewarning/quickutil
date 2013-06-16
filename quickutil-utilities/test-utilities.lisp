919764b827e7abeca417d4b1f193de9e7dfb173f
Eitarow Fukamachi <e.arrows@gmail.com>
2013-06-16 16:09:24 -0700

    made some changes to quickutil-utilities-test.

--- quickutil-utilities/test-utilities.lisp
+++ quickutil-utilities/test-utilities.lisp
@@ -7,24 +7,30 @@
    :bordeaux-threads))
 (in-package :quickutil-utilities.test)
 
+(quickutil-server:stop)
 (quickutil-server:start :port 18081)
 
 (quickutil-client:set-quickutil-host "localhost:18081")
 
+#+thread-support
 (loop with i = 0
       with log-stream = *standard-output*
-      with lock = (bordeaux-threads:make-lock "UTILIZE")
       for name in (quickutil-utilities:all-utilities)
+      for thread = (bordeaux-threads:make-thread
+                    #'(lambda ()
+                        (format log-stream "~&Testing for: ~A~%" name)
+                        (let ((*error-output* log-stream)
+                              (*standard-output* log-stream))
+                          (handler-case (quickutil-client:utilize name)
+                            (error (e) (princ e log-stream))))))
       do
    (incf i)
-   (format t "~&Testing for: ~A~%" name)
-   (bordeaux-threads:with-lock-held (lock)
-    (let ((*error-output* log-stream)
-          (*standard-output* log-stream)
-          )
-      (handler-case (quickutil-client:utilize name)
-        (error (e) (princ e log-stream)))))
+   (loop while (bordeaux-threads:thread-alive-p thread)
+         do (sleep 0.3))
       finally
       (format t "~&~A utilities were tested.~%" i))
+
+#-thread-support
+(error "Your Lisp doesn't support threads.")
 
 (quickutil-server:stop)
