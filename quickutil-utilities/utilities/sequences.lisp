944b9c127c998d2cda87da9874b258ee3fa20352
Robert Smith <quad@symbo1ics.com>
2013-06-17 14:59:26 -0700

    Add PARTITION-IF(-NOT) and EQUIVALENCE-CLASSES. Improve documentation.

--- quickutil-utilities/utilities/sequences.lisp
+++ quickutil-utilities/utilities/sequences.lisp
@@ -3,7 +3,7 @@
 (defutil sort-copy (:version (1 . 0)
                     :provides (sort-copy stable-sort-copy)
                     :category sequences)
-  "Copying versions of SORT and STABLE-SORT."
+  "Copying versions of `cl:sort` and `cl:stable-sort`."
   #>%%%>
   (defun sort-copy (sequence predicate &key key)
     "Sort a copy of SEQUENCE according to PREDICATE accessing the
@@ -20,7 +20,7 @@
 
 (defutil take (:version (1 . 0)
                :category sequences)
-  "Take the first N elements from SEQUENCE."
+  "Take the first `n` elements from `sequence`."
   #>%%%>
   (defun take (n sequence)
     %%DOC
@@ -29,7 +29,7 @@
 
 (defutil drop (:version (1 . 0)
                :category sequences)
-  "Drop the first N elements from SEQUENCE."
+  "Drop the first `n` elements from `sequence`."
   #>%%%>
   (defun drop (n sequence)
     %%DOC
@@ -39,7 +39,7 @@
 
 (defutil subdivide (:version (1 . 0)
                     :category sequences)
-  "Split SEQUENCE into subsequences of size CHUNK-SIZE."
+  "Split `sequence` into subsequences of size `chunk-size`."
   #>%%%>
   (defun subdivide (sequence chunk-size)
     %%DOC
@@ -67,7 +67,7 @@
 (defutil n-grams (:version (1 . 0)
                   :depends-on take
                   :category sequences)
-  "Find all N-grams of the sequence SEQUENCE."
+  "Find all `n`-grams of the sequence `sequence`."
   #>%%%>
   (defun n-grams (n sequence)
     %%DOC
@@ -83,4 +83,64 @@
       ;; General sequences
       (sequence (loop :for i :below (1+ (- (length sequence) n))
                       :collect (subseq sequence i (+ i n))))))
+  %%%)
+
+(defutil partition-if (:version (1 . 0)
+                       :category (sequences functional)
+                       :provides (partition-if partition-if-not))
+  "Partition sequences based off of a predicate."
+  #>%%%>
+  (defun partition-if (f seq)
+    "Given a predicate F, partition SEQ into two sublists, the first
+of which has elements that satisfy F, the second which do not."
+    (let ((yes nil)
+          (no nil))
+      (map nil #'(lambda (x)
+                   (if (funcall f x)
+                       (push x yes)
+                       (push x no))))
+      (values yes no)))
+  
+  (defun partition-if-not (f seq)
+    "Partition SEQ into two sublists, the first whose elements do not
+satisfy the predicate F, and the second whose elements do."
+    (multiple-value-bind (yes no)
+        (partition-if f seq)
+      (values no yes)))
+  %%%)
+
+(defutil equivalence-classes (:version (1 . 0)
+                              :category (sequences functional))
+  "Partition the sequence `seq` into a vector of equivalence classes
+defined by the equivalence relation `equiv`."
+  #>%%%>
+  (defun equivalence-classes (equiv seq)
+    %%DOC
+    (let ((half-length (floor (length seq) 2))
+          (classes nil))
+      (labels ((find-equivalence-class (x)
+                 (find-if (lambda (class)
+                            (funcall equiv (aref class 0) x))
+                          classes))
+               
+               (new-class (x)
+                 (make-array half-length
+                             :initial-element x
+                             :adjustable t
+                             :fill-pointer 1))
+               
+               (add-to-class (x)
+                 (let ((class (find-equivalence-class x)))
+                   (if class
+                       (vector-push-extend x class)
+                       (push (new-class x) classes)))))
+        (declare (inline find-equivalence-class
+                         new-class
+                         add-to-class))
+        
+        ;; Partition into equivalence classes.
+        (map nil #'add-to-class seq)
+        
+        ;; Return the classes.
+        classes)))
   %%%)
