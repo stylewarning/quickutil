56e46eb905a583270e12ab1bc92d14646e527814
Robert Smith <quad@symbo1ics.com>
2013-06-17 16:08:17 -0700

    Improve aesthetics of docs for SYNTAX-BIND.

--- quickutil-utilities/utilities/syntax.lisp
+++ quickutil-utilities/utilities/syntax.lisp
@@ -3,14 +3,14 @@
 ;; Author: Matt Niemeir (github: m-n)
 (defutil syntax-bind (:version (1 . 0)
                       :category (syntax reader))
-  "Set `*readtable*` to a copy of readtable-expression and modify the copy.
+  "Set `*readtable*` to a copy of `readtable-expression` and modify the copy.
 Because `cl:load` and `cl:compile-file` bind `*readtable*`, this effectively gives
 a file local readtable in the same way `cl:in-package` gives a file local binding
 to `*package*`. Example call:
 
     (syntax-bind *convenient-readtable*
-      (#\# #\@) 'send-line-to-channel
-      #\!       'not-reader)
+      (#\\# #\\@) 'send-line-to-channel
+      #\\!       'not-reader)
 
   Sets `#\!` to a nonterminating macro character. Use `(#\!)` to set it to 
   a terminating macro character. A null `readtable-expression` creates a readtable
