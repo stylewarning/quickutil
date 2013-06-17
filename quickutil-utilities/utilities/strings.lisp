c5856f0c6cddf334de992b83807c06735914036b
Robert Smith <quad@symbo1ics.com>
2013-06-17 15:58:35 -0700

    Add SEPARATED-STRING-APPEND, STRING-APPEND-SPACE. Supersedes Issue #10

--- quickutil-utilities/utilities/strings.lisp
+++ quickutil-utilities/utilities/strings.lisp
@@ -16,3 +16,49 @@
     (string-append* strings))
   %%%)
 
+(defutil separated-string-append (:version (1 . 0)
+                                  :provides (separated-string-append
+                                             separated-string-append*)
+                                  :category strings)
+  "Append strings or a sequence of strings, separated by a separator."
+  #>%%%>
+  (defun separated-string-append* (separator sequence-of-strings)
+    "Concatenate all of the strings in SEQUENCE-OF-STRINGS separated
+    by the string SEPARATOR."
+    (typecase sequence-of-strings
+      (null "")
+      
+      (cons (with-output-to-string (*standard-output*)
+              (mapl #'(lambda (tail)
+                        (write-string (car tail))
+                        (unless (null (cdr tail))
+                          (write-string separator)))
+                    sequence-of-strings)))
+      
+      (sequence
+       (let ((length (length sequence-of-strings)))
+         (with-output-to-string (*standard-output*)
+           (map nil #'(lambda (string)
+                        (write-string string)
+                        (unless (zerop (decf length))
+                          (write-string separator)))
+                sequence-of-strings))))
+      
+      (t (error 'type-error :expected-type 'sequence
+                            :datum sequence-of-strings))))
+  
+  (defun separated-string-append (separator &rest strings)
+    "Concatenate the strings STRINGS separated by the string
+SEPARATOR."
+    (separated-string-append* separator strings))
+  %%%)
+
+(defutil string-append-space (:version (1 . 0)
+                              :depends-on separated-string-append
+                              :category strings)
+  "Concatenate `strings` separated by a single space."
+  #>%%%>
+  (defun string-append-space (&rest strings)
+    %%DOC
+    (separated-string-append* " " strings))
+  %%%)
