9e458b4c09a556fe23c1c4e0edbc3afaaaa86e7a
Robert Smith <quad@symbo1ics.com>
2013-06-17 19:34:55 -0700

    Add ENSURE-KEYWORD utility.

--- quickutil-utilities/utilities/symbols.lisp
+++ quickutil-utilities/utilities/symbols.lisp
@@ -9,3 +9,16 @@
     (string= (symbol-name a)
              (symbol-name b)))
   %%%)
+
+(defutil ensure-keyword (:version (1 . 0)
+                         :category symbols)
+  "Ensure that a keyword is returned for the string designator `x`."
+  #>%%%>
+  (defun ensure-keyword (x)
+    %%DOC
+    (nth-value 0 (etypecase x
+                   (keyword   x)
+                   (symbol    (intern (symbol-name x) :keyword))
+                   (string    (intern x :keyword))
+                   (character (intern (string x) :keyword)))))
+  %%%)
