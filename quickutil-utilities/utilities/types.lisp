1bf3892027756c15df62b5d313d4997ae24cb3bc
Robert Smith <quad@symbo1ics.com>
2013-07-02 06:02:29 -0700

    Delete multioctet and generalize octet.

--- quickutil-utilities/utilities/types.lisp
+++ quickutil-utilities/utilities/types.lisp
@@ -2,25 +2,13 @@
 
 (defutil octet (:version (1 . 0)
                 :category (types low-level))
-  "A type representing an octet: 8 bits. This can often be used for
-optimization in Common Lisp."
+  "A type representing an octet: 8 bits. If a positive integer `n` is
+specified, then `(octet n)` represents `8n` bits. This can often be
+used for optimization in Common Lisp."
   #>%%%>
-  (deftype octet ()
-    %%DOC
-    '(unsigned-byte 8))
-  %%%)
-
-(defutil multioctet (:version (1 . 0)
-                     :category (types low-level))
-  "A type representing several octets: `8n` bits for positive integer
-`n`. This can often be used for optimization in Common Lisp.
-
-Note the equivalence:
-
-    (multioctet 1) == octet"
-  #>%%%>
-  (deftype multioctet (n)
+  (deftype octet (&optional (n 1))
     %%DOC
     (check-type n (integer 1))
     `(unsigned-byte ,(* 8 n)))
   %%%)
+
