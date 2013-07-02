f4f5feecec2ca36b3dc7903589bf4569089c34fe
Robert Smith <quad@symbo1ics.com>
2013-07-01 20:53:07 -0700

    Add multioctet

--- quickutil-utilities/utilities/types.lisp
+++ quickutil-utilities/utilities/types.lisp
@@ -9,3 +9,18 @@ optimization in Common Lisp."
     %%DOC
     '(unsigned-byte 8))
   %%%)
+
+(defutil multioctet (:version (1 . 0)
+                     :category (types low-level))
+  "A type representing several octets: `8n` bits for positive integer
+`n`. This can often be used for optimization in Common Lisp.
+
+Note the equivalence:
+
+    (multioctet 1) == octet"
+  #>%%%>
+  (deftype multioctet (n)
+    %%DOC
+    (check-type n (integer 1))
+    `(unsigned-byte ,(* 8 n)))
+  %%%)
