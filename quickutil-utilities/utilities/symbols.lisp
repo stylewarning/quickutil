43e447085f7aad6aba98137cfd4c6e120a6e8b8b
Robert Smith <quad@symbo1ics.com>
2013-07-04 18:32:41 -0700

    Improve SAME-NAME and ENSURE-KEYWORD.
    Thanks to stassats.

--- quickutil-utilities/utilities/symbols.lisp
+++ quickutil-utilities/utilities/symbols.lisp
@@ -6,8 +6,9 @@
   #>%%%>
   (defun same-name (a b)
     %%DOC
-    (string= (symbol-name a)
-             (symbol-name b)))
+    (check-type a symbol)
+    (check-type b symbol)
+    (string= a b))
   %%%)
 
 (defutil ensure-keyword (:version (1 . 0)
@@ -16,9 +17,6 @@
   #>%%%>
   (defun ensure-keyword (x)
     %%DOC
-    (nth-value 0 (etypecase x
-                   (keyword   x)
-                   (symbol    (intern (symbol-name x) :keyword))
-                   (string    (intern x :keyword))
-                   (character (intern (string x) :keyword)))))
+    
+    (values (intern (string x) :keyword)))
   %%%)
