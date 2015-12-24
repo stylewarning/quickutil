4706e5971bc81e0e448bdc0423853f5762e98d84
Robert Smith <quad@symbo1ics.com>
2015-12-23 21:47:12 -0800

    Revert "Add DEFINE-ABBREVIATION per Issue #43"
    This reverts commit 92cb96930ca47731c4083f48f39191e4cd27b2bc.

--- quickutil-utilities/utilities/synonyms.lisp
+++ quickutil-utilities/utilities/synonyms.lisp
@@ -1,44 +1,5 @@
 (in-package #:quickutil-utilities.utilities)
 
-(defutil define-abbreviation (:version (1 . 0)
-                              :category (synonyms misc))
-  "Abbreviate LONG macro, function, or special operator name as SHORT. If LAMBDA-LIST is present, also copy appropriate SETF-expander."
-  #>%%%>
-  (defmacro define-abbreviation (short long &optional lambda-list)
-    %%DOC
-    (if (special-operator-p long)
-        `(defmacro ,short (&rest args)
-           `(,',long ,@args))
-        `(eval-when (:compile-toplevel :execute :load-toplevel)
-           (cond
-             ((macro-function ',long)
-              (setf (macro-function ',short) (macro-function ',long)))
-             ((fboundp ',long)
-              (setf (fdefinition ',short) (fdefinition ',long))
-              ,(when lambda-list
-                 `(define-setf-expander ,short ,(append lambda-list)
-                    (values ,@(multiple-value-bind
-                                    (dummies vals store store-form access-form)
-                                  (get-setf-expansion
-                                   (cons long (remove-if (lambda (sym)
-                                                           (member sym '(&optional &key)))
-                                                         lambda-list)))
-                                (let ((expansion-vals (mapcar (lambda (x) `(quote ,x))
-                                                              (list dummies
-                                                                    vals
-                                                                    store
-                                                                    store-form
-                                                                    access-form))))
-                                  (setf (second expansion-vals)
-                                        (cons 'list vals))
-                                  expansion-vals))))))
-             (t
-              (error "Can't abbreviate ~a" ',long)))
-           (setf (documentation ',short 'function) (documentation ',long 'function))
-           ',short)))
-  %%%
-  )
-
 (defutil true-false (:version (1 . 0)
                      :provides (true false)
                      :category (constants synonyms))
