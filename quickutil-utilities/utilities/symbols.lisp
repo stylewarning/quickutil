314737a9221c61d460a6c9f665665abb97037f02
Robert Smith <quad@symbo1ics.com>
2015-12-23 20:42:41 -0800

    Add MKSTR and SYMB per Issue #39
    Proposed by @EuAndreh, originally written in On Lisp by Paul Graham.

--- quickutil-utilities/utilities/symbols.lisp
+++ quickutil-utilities/utilities/symbols.lisp
@@ -19,3 +19,29 @@
     %%DOC
     (values (intern (string x) :keyword)))
   %%%)
+
+(defutil mkstr (:version (1 . 0)
+                :category (strings on-lisp))
+  "Receives any number of objects (string, symbol, keyword, char, number), extracts all printed representations, and concatenates them all into one string.
+
+Extracted from _On Lisp_, chapter 4."
+  #>%%%>
+  (defun mkstr (&rest args)
+    %%DOC
+    (with-output-to-string (s)
+      (dolist (a args) (princ a s))))
+  %%%)
+
+(defutil symb (:version (1 . 0)
+               :depends-on mkstr
+               :category (symbols on-lisp))
+  "Receives any number of objects, concatenates all into one string with `#'mkstr` and converts them to symbol.
+
+Extracted from _On Lisp_, chapter 4.
+
+See also: `symbolicate`"
+  #>%%%>
+  (defun symb (&rest args)
+    %%DOC
+    (values (intern (apply #'mkstr args))))
+  %%%)
