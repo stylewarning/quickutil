910a2bd989ad88e675ef6527b088479c16e1f1ca
Mark Skilbeck <mgsk@moon.home>
2018-07-17 19:25:22 +0100

    Add RANDOM-FROM and BISECT

--- quickutil-utilities/utilities/random.lisp
+++ quickutil-utilities/utilities/random.lisp
@@ -21,3 +21,20 @@
         (+ a (random (- (1+ b) a)))))
   %%%)
 
+(defutil random-from (:version (1 . 0)
+                      :depends-on bisect
+                      :category random)
+  "Take N from CHOICES sampled according to :WEIGHTS if provided,
+otherwise each of CHOICES is equally likely."
+  #>%%%>
+  (defun random-from (choices &key weights (n 1))
+    %%DOC
+    (let* ((len (length choices))
+           (weights (or weights (make-list len :initial-element (float (/ len)))))
+           (cumm (loop for i in weights for c = i then (+ c i) collect c))
+           (total (first (last cumm))))
+      (loop repeat n
+            collect (nth (bisect cumm (random total) :low 0 :high (1- len))
+                         choices))))
+  %%%)
+
--- quickutil-utilities/utilities/sequences.lisp
+++ quickutil-utilities/utilities/sequences.lisp
@@ -155,3 +155,25 @@ each element of the sequence and executing `body`. Return the value
             ,seq)
        ,return))
   %%%)
+
+(defutil bisect (:version (1 . 0)
+                 :category sequences)
+  "Find position in ordered LIST where ELT would be inserted such that
+the result remains ordered. The keyword arguments :LOW and :HIGH are
+used to specify the region of LIST in which to search for the position.
+
+Note that the position will be a number X such that all positions in
+LIST before X have (<= (NTH X LIST) ELT). This results in the position
+being _after_ any elements that match ELT. For example
+    (BISECT '(1 2 2 3) 2) => 3."
+  #>%%%>
+  (defun bisect (list elt &key (low 0) high)
+    %%DOC
+    (let* ((high (or high (length list)))
+           (mid (floor (/ (+ low high) 2))))
+      (if (>= low high)
+          low
+          (if (< elt (nth mid list))
+              (bisect list elt :low low :high mid)
+              (bisect list elt :low (1+ mid) :high high)))))
+  %%%)
